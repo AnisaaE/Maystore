2a16cfb34543ba120486105c5f7d07f515821703
Anisa <anisaemin97@gmail.com>
2024-08-20 20:26:45 +0300

    rewrite the nav-bar

--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -1,131 +1,217 @@
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import "./style.css";
 
 function NavigationBar() {
   return (
-    <Navbar expand="lg" className="navbar-custom p-3 bg-custom-purple ">
-      <Container fluid className="montserrat">
-        <Navbar.Brand href="/">
+    <nav className="navbar navbar-expand-lg navbar-custom p-3 bg-custom-purple">
+      <div className="container-fluid montserrat">
+        <a className="navbar-brand" href="/">
           <img
             src={process.env.PUBLIC_URL + "/logo.png"}
             alt="Maystore Logo"
-            className="img-fluid d-inline-block align-top logo "
+            className="img-fluid d-inline-block align-top logo"
           />
-        </Navbar.Brand>
-        <Form className="d-flex me-4 ">
-          <Form.Control
+        </a>
+        <form className="d-flex me-4">
+          <input
+            className="form-control rounded-start-pill"
             type="search"
             placeholder="Търсене"
             aria-label="Search"
-            className="rounded-start-pill"
           />
-          <Button
-            variant="outline-light"
-            className="search-icon ms-0 rounded-end-pill"
-          >
-            <i class="bi bi-search"></i>
-          </Button>
-        </Form>
-
-        <Navbar.Toggle
-          aria-controls="navbarScroll row"
-          className="border border-white border border-3"
-        />
-
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-            <NavDropdown
-              title="Бизнес клиенти"
-              id="navbarScrollingDropdown"
-              className="dropdown-hover custom-dropdown montserrat "
-            >
+          <button className="btn search-icon ms-0 rounded-end-pill border border-white" type="submit">
+            <i className="bi bi-search search-icon"></i>
+          </button>
+        </form>
+        <button
+          className="navbar-toggler border border-white border-3"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarScroll">
+          <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle dropdown-hover custom-dropdown montserrat"
+                href="#"
+                id="navbarScrollingDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Бизнес клиенти
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
               
               <div className="dropdown-column">
-                <NavDropdown.Item href="/Принтове" className="header-css">
-                  Принтове
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action3">
-                  "Тениска на Месеца"
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  "Избери Любовта"
-                </NavDropdown.Item>
-              </div>
-              <div className="dropdown-column">
-              <NavDropdown.Item href="/Принтове" className="header-css">Тениски</NavDropdown.Item>
-                <NavDropdown.Item href="#action5">
-                  Тениски без Принтове
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action6">
-                  Макси Тениски
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action7">
-                  Тениски с Подплънки
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action8">
-                  Поло Тениски
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action9">Промоции</NavDropdown.Item>
-              </div>
-
-              <div className="dropdown-column col">
-                 <NavDropdown.Item href="/Принтове" className="header-css">Аксесоари</NavDropdown.Item>
-                <NavDropdown.Item href="#action10">Шапки</NavDropdown.Item>
-                <NavDropdown.Item href="#action11">Чанти</NavDropdown.Item>
-              </div>
-              
-            </NavDropdown>
-
-            <NavDropdown
-              title="Индивидуални клиенти"
-              id="navbarScrollingDropdown"
-              className="custom-dropdown dropdown-hover "
-            >
-              <div className="dropdown-column">
-                <NavDropdown.Header>Prints</NavDropdown.Header>
-                <NavDropdown.Item href="#action3">
-                  "Тениска на Месеца"
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  "Избери Любовта"
-                </NavDropdown.Item>
-              </div>
-              <div className="dropdown-column">
-                <NavDropdown.Header>T-shirts</NavDropdown.Header>
-                <NavDropdown.Item href="#action5">
-                  Тениски без Принтове
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action6">
-                  Макси Тениски
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action7">
-                  Тениски с Подплънки
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action8">
-                  Поло Тениски
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action9">Промоции</NavDropdown.Item>
-              </div>
-              <div className="dropdown-column">
-                <NavDropdown.Header>Accessories</NavDropdown.Header>
-                <NavDropdown.Item href="#action10">Шапки</NavDropdown.Item>
-                <NavDropdown.Item href="#action11">Чанти</NavDropdown.Item>
-              </div>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-
-        <i
-          className="bi bi-basket-fill"
-          style={{ fontSize: "1.5rem", color: "white" }}
-        ></i>
-      </Container>
-    </Navbar>
+                  <li>
+                    <a className="dropdown-item dropdown-header" href="/Принтове">
+                      Принтове
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action3">
+                      "Тениска на Месеца"
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action4">
+                      "Избери Любовта"
+                    </a>
+                  </li>
+                </div>
+                <div className="dropdown-column">
+                  <li>
+                    <a className="dropdown-item dropdown-header" href="/Принтове">
+                      Принтове
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action3">
+                      "Тениска на Месеца"
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action4">
+                      "Избери Любовта"
+                    </a>
+                  </li>
+                </div>
+                <div className="dropdown-column">
+                  <li>
+                    <a className="dropdown-item dropdown-header" href="/Принтове">
+                      Тениски
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action5">
+                      Тениски без Принтове
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action6">
+                      Макси Тениски
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action7">
+                      Тениски с Подплънки
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action8">
+                      Поло Тениски
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action9">
+                      Промоции
+                    </a>
+                  </li>
+                </div>
+                <div className="dropdown-column">
+                  <li>
+                    <a className="dropdown-item dropdown-header" href="/Принтове">
+                      Аксесоари
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action10">
+                      Шапки
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action11">
+                      Чанти
+                    </a>
+                  </li>
+                </div>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle dropdown-hover custom-dropdown"
+                href="#"
+                id="navbarScrollingDropdown2"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Индивидуални клиенти
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown2">
+                <div className="dropdown-column">
+                  <li>
+                    <span className="dropdown-header">Prints</span>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action3">
+                      "Тениска на Месеца"
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action4">
+                      "Избери Любовта"
+                    </a>
+                  </li>
+                </div>
+                <div className="dropdown-column">
+                  <li>
+                    <span className="dropdown-header">T-shirts</span>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action5">
+                      Тениски без Принтове
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action6">
+                      Макси Тениски
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action7">
+                      Тениски с Подплънки
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action8">
+                      Поло Тениски
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action9">
+                      Промоции
+                    </a>
+                  </li>
+                </div>
+                <div className="dropdown-column">
+                  <li>
+                    <span className="dropdown-header">Accessories</span>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action10">
+                      Шапки
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#action11">
+                      Чанти
+                    </a>
+                  </li>
+                </div>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <i className="bi bi-basket-fill" style={{ fontSize: "1.5rem", color: "white" }}></i>
+      </div>
+    </nav>
   );
 }
 
